b0a6055c3f1c934a60a5dce2015f72d234086951
phpboke <phpboke@163.com>
2018-06-22 21:31:38 +0800

    sql

---
+++ src/sql/ssm.sql
@@ -0,0 +1,109 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 192.168.200.11
+Source Server Version : 50622
+Source Host           : 192.168.200.11:3306
+Source Database       : ssm
+
+Target Server Type    : MYSQL
+Target Server Version : 50622
+File Encoding         : 65001
+
+Date: 2018-06-22 21:30:04
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for items
+-- ----------------------------
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL COMMENT '商品名称',
+  `price` float(10,1) NOT NULL COMMENT '商品定价',
+  `detail` text COMMENT '商品描述',
+  `pic` varchar(64) DEFAULT NULL COMMENT '商品图片',
+  `createtime` datetime NOT NULL COMMENT '生产日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of items
+-- ----------------------------
+INSERT INTO `items` VALUES ('1', 'sss123456789', '3000.0', 'ssss2', null, '2015-02-06 13:23:02');
+INSERT INTO `items` VALUES ('2', 'vivi22234', '6002.0', '照亮你的美ffff', null, '2015-02-09 13:22:57');
+INSERT INTO `items` VALUES ('3', '背包d', '200.0', '333名牌背包，容量大质量好！！！！', null, '2015-02-06 13:23:02');
+INSERT INTO `items` VALUES ('4', 'Mac Pro', '9080.0', '专业开发笔记本电脑，mac', null, '2018-05-22 22:39:18');
+INSERT INTO `items` VALUES ('5', 'Mac air', '6800.0', '娱乐笔记本', null, '2018-05-26 22:42:17');
+
+-- ----------------------------
+-- Table structure for orderdetail
+-- ----------------------------
+DROP TABLE IF EXISTS `orderdetail`;
+CREATE TABLE `orderdetail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orders_id` int(11) NOT NULL COMMENT '订单id',
+  `items_id` int(11) NOT NULL COMMENT '商品id',
+  `items_num` int(11) DEFAULT NULL COMMENT '商品购买数量',
+  PRIMARY KEY (`id`),
+  KEY `FK_orderdetail_1` (`orders_id`),
+  KEY `FK_orderdetail_2` (`items_id`),
+  CONSTRAINT `FK_orderdetail_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_orderdetail_2` FOREIGN KEY (`items_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orderdetail
+-- ----------------------------
+INSERT INTO `orderdetail` VALUES ('1', '3', '1', '1');
+INSERT INTO `orderdetail` VALUES ('2', '3', '2', '3');
+INSERT INTO `orderdetail` VALUES ('3', '4', '3', '4');
+INSERT INTO `orderdetail` VALUES ('4', '4', '2', '3');
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL COMMENT '下单用户id',
+  `number` varchar(32) NOT NULL COMMENT '订单号',
+  `createtime` datetime NOT NULL COMMENT '创建订单时间',
+  `note` varchar(100) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `FK_orders_1` (`user_id`),
+  CONSTRAINT `FK_orders_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+INSERT INTO `orders` VALUES ('3', '1', '1000010', '2015-02-04 13:22:35', null);
+INSERT INTO `orders` VALUES ('4', '1', '1000011', '2015-02-03 13:22:41', null);
+INSERT INTO `orders` VALUES ('5', '10', '1000012', '2015-02-12 16:13:23', null);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL COMMENT '用户名称',
+  `birthday` date DEFAULT NULL COMMENT '生日',
+  `sex` char(1) DEFAULT NULL COMMENT '性别',
+  `address` varchar(256) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '王五', null, '2', null);
+INSERT INTO `user` VALUES ('10', '张三', '2014-07-10', '1', '北京市');
+INSERT INTO `user` VALUES ('16', '张小明', null, '1', '河南郑州');
+INSERT INTO `user` VALUES ('22', '陈小明', null, '1', '河南郑州');
+INSERT INTO `user` VALUES ('24', '张三丰', null, '1', '河南郑州');
+INSERT INTO `user` VALUES ('25', '陈小明', null, '1', '河南郑州');
+INSERT INTO `user` VALUES ('26', '王五', null, null, null);
